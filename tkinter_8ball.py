f74cddc28d121118f7bd67c040da3497e13763ef
sdemirjian <sdemirjian@fwparker.org>
2017-04-26 15:28:31 -0500

    yee

--- tkinter_8ball.py
+++ tkinter_8ball.py
@@ -12,3 +12,29 @@
 # - Add a Label widget to display the answer (set to a initial value of "Your Fortune Here" or "--" or similar)
 # - Get your random answer message from a list of at least 10 possible strings. (e.g. ["Yes", "No", "Most Likely", "Definitely", etc...])
 # - Add THREE or more other style modifications to make your app unique (font family, font size, color, padding, image, borders, justification, whatever you can find in tkinter library etc.)  Make a comment at the top or bottom of your code to tell me your 3 things you did. (Just to help me out in checking your assignment)
+from tkinter import *
+from tkinter import font
+import random
+
+class App():
+    def __init__(self, master):
+        #variables
+        self.response = StringVar()
+        self.list = ["Sure, why not", "Who cares?", "There can be no question.", "Definitely not", "Probably", "Try again later", "Only on Tuesdays", "Yes", "No", "It is certain", "That's the dumbest question I've ever been asked don't even think about asking that again or I swear to God I'll find where you live and make sure it never happens again."]
+
+        #Labels
+        self.intro_label = Label(master, text="Ask the 8-Ball a question!", bg="gold").grid(row=1, column=1, columnspan=4, sticky="e" + "w")
+        self.response_label = Label(master, textvariable=self.response).grid(row=4, column=1, columnspan=4, sticky="e" + "w")
+
+        #Entry:
+        self.entry = Entry(master, text="Enter your question here.").grid(row=2, column=1, columnspan=4, sticky="e" + "w")
+
+        #Buttons
+        self.receive_fortune = Button(master, text="Receive Fortune", relief="raised", bg="blue", fg="yellow", command=lambda:self.response.set(self.list[random.randrange(len(self.list))])).grid(row=3, column=1, columnspan=4, sticky="e" + "w")
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Magic 8-Ball")
+    my_app = App(root)
+    root.mainloop()
--- tkinter_universal_gravity_calculator.py
+++ tkinter_universal_gravity_calculator.py
@@ -18,4 +18,41 @@
 # - When "Calculate" is pushed, the result is displayed.  Yay!
 # - Add an exception for the possible entry of zero radius (ZeroDivisionError Exception)
 # - Make your app unique by changing 3 or more DIFFERENT style attributes or kwargs for your app.  Perhaps consider: fonts, color, padding, widths, etc).  Put a comment in your code to tell me what you changed. If you change the font for all the widgets, that is ONE thing.  If you add padx to all your app widgets, that is ONE thing.  If you change the color of all your blocks, that is ONE thing.
+from tkinter import *
+from tkinter import font
 
+class App():
+    def __init__(self, master):
+        self.font = font.Font(family="Helvetica Light", size=10)
+        self.result = StringVar()
+        self.result.set(3)
+        self.m1_variable = DoubleVar()
+        self.m2_variable = DoubleVar()
+        self.r_variable = DoubleVar()
+
+        #labels
+        self.title = Label(master, text="Gravitational Force Calculator", font=self.font).grid(row=1, column=1, columnspan=4, sticky="e" + "w")
+        self.m1 = Label(master, text="M1:", width=15, font=self.font, fg="blue").grid(row=2, column=1, columnspan=2, sticky="e" + "w")
+        self.m2 = Label(master, text="M2:", font=self.font, fg="blue").grid(row=3, column=1, columnspan=2, sticky="e" + "w")
+        self.r = Label(master, text="R:", font=self.font, fg="blue").grid(row=4, column=1, columnspan=2, sticky="e" + "w")
+        self.result_label = Label(master, textvariable=self.result, font=self.font).grid(row=5, column=3, columnspan=2, sticky="e" + "w")
+
+        #entries
+        self.m1_entry = Entry(master, textvariable=self.m1_variable).grid(row=2, column=3, columnspan=2, sticky="e" + "w")
+        self.m2_entry = Entry(master, textvariable=self.m2_variable).grid(row=3, column=3, columnspan=2, sticky="e" + "w")
+        self.r_entry = Entry(master, textvariable=self.r_variable).grid(row=4, column=3, columnspan=2, sticky="e" + "w")
+
+        #buttons
+        self.calculate_button = Button(master, text="Calculate Force", bg="orange", command=lambda:self.result.set(self.calculate(self.m1_variable.get(), self.m2_variable.get(), self.r_variable.get())), font=self.font).grid(row=5, column=1, columnspan=2, sticky="e" + "w")
+
+    def calculate(self, m1, m2, r):
+        if r**2 == 0:
+            return "Division by zero!"
+        else:
+            return (6.67e-11) * (m1 * m2) / r**2
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Universal Gravity Calculator")
+    my_app = App(root)
+    root.mainloop()
